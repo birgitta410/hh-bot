38ce3d18d5ba61516d8ec4fc1b2469fc383467f9
Birgitta B <bboeckel@thoughtworks.com>
2017-02-26 11:21:13 +0100

    Spec format for conversation specs

--- spec/conversationSpec.js
+++ spec/conversationSpec.js
@@ -3,9 +3,16 @@ describe('conversation', () => {
 
   const conversation = require('../bot/conversation');
 
+  function spec(input, responseText, responseOptions) {
+    const response = conversation.respond({ message: { message: { text: input }}});
+    expect(response.text).toBe(responseText);
+    expect(response.options).toEqual(responseOptions);
+  }
+
   it('should respond to /start', () => {
-    const response = conversation.respond({ message: { message: { text: '/start' }}});
-    expect(response.text).toBe('Hello, this is Hacking Health! What are you looking for?');
+    spec('/start', 
+      'Hello, this is Hacking Health! What are you looking for?',
+      [['I need some advice on a skin condition.'], ['Nothing, thanks, I ended up here by accident.']]);
   });
 
 });
